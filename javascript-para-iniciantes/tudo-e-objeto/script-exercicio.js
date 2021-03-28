df142f5304195f59607eca33cc9ca3bb54906b38
José Carlos <jjc.silva10@gmail.com>
2021-03-28 12:15:17 -0300

    Adicionando arquivos da aula 7

---
+++ javascript-para-iniciantes/tudo-e-objeto/script-exercicio.js
@@ -0,0 +1,18 @@
+//Exercícios
+
+//nomeie 3 propriedades ou métodos de strings
+var animal = 'Macaco'
+animal.length();
+animal.fixed();
+animal.replace();
+
+//nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn');
+btn.addEventListener();
+btn.appendChild();
+btn.innerHTML();
+btn.id();
+btn.outerHTML();
+
+//busque na web um objeto (método) capaz de interagir com o clipboard(parte do seu computador que lida com o CTRL + C e CTRL + V)
+//Site clipboard.js
